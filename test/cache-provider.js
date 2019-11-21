73ba89f5fe301141254df9b35263c42e4cff9da7
dragonman225 <zxcvb22217@gmail.com>
2019-11-21 21:28:08 +0800

    refactor: Update test for cache-provider

--- test/cache-provider.js
+++ test/cache-provider.js
@@ -1,26 +1,33 @@
 const fs = require('fs')
+const fsPromises = fs.promises
 const path = require('path')
+const { test } = require('zora')
 
 const CacheProvider = require('../src/cache-provider')
 const notionCacheDir = path.join(__dirname, 'notion')
 
-/** Prepare */
-fs.mkdirSync(notionCacheDir, { recursive: true })
+test('', async t => {
+  /** Prepare */
+  await fsPromises.mkdir(notionCacheDir, { recursive: true })
 
-/** Main */
-const c = new CacheProvider(__dirname)
+  /** Main */
+  const c = new CacheProvider(__dirname)
 
-/** \ and / will be ignored */
-const uri = 'notion://123/456\\789'
+  /** \ and / will be ignored */
+  const uri = 'notion://123/456\\789'
 
-/** This will fail */
-console.log(c.get(uri))
+  /** This will fail */
+  let cache = await c.get(uri)
+  t.equal(typeof cache, 'undefined', 'should be undefined')
 
-/** Write something */
-c.set(uri, { test: '123123' })
+  /** Write something */
+  const cacheData = { test: '123123' }
+  await c.set(uri, cacheData)
 
-/** This will succeed */
-console.log(c.get(uri))
+  /** This will succeed */
+  cache = await c.get(uri)
+  t.deepEqual(cache, cacheData, 'should get an object')
 
-/** Cleanup */
-fs.rmdirSync(notionCacheDir, { recursive: true })
+  /** Cleanup */
+  fsPromises.rmdir(notionCacheDir, { recursive: true })
+})
